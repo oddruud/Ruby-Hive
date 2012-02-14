82dd11f5f8582034b04d0417c1f441541bd0f908
Ruud op den Kelder <ruudopdenkelder@Ruuds-MacBook-Air.local>
2012-02-14 09:26:42 +0700

    removed more artifacts

--- lib/hive/Common/GraphicsView/viewconstants.rb
+++
@@ -1,9 +0,0 @@
-require 'gosu'
-
-class Hive::ViewConstants
-	WINDOW_WIDTH = 1500
-	WINDOW_HEIGHT = 768
-	
- 	TOP_COLOR = Gosu::Color.new(0xFF1EB1FA)
-	BOTTOM_COLOR = Gosu::Color.new(0xFF1D4DB5)
-end 
