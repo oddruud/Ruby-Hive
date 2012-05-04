07a57763e61437a65e53f6ea26ce276cd7d6c213
oddruud <oddruud@gmail.com>
2012-05-04 17:00:40 +0200

    added some graphics

--- lib/hive/common/graphics_view/game_view.rb
+++ lib/hive/common/graphics_view/game_view.rb
@@ -2,6 +2,8 @@ require 'gosu'
 require 'chingu'
 
 class Hive::GameView < Chingu::Window
+
+  @@BASE_RESOURCE_PATH = "../resources"
   
   def initialize()
     super(Hive::ViewConstants::WINDOW_WIDTH, Hive::ViewConstants::WINDOW_HEIGHT, false)
--- lib/hive/common/graphics_view/game_view_steps.rb
+++ lib/hive/common/graphics_view/game_view_steps.rb
@@ -1,9 +1,19 @@
 class Hive::GameStepView < Hive::GameView 
   
+attr_reader  :next
+attr_reader  :previous
+attr_reader  :skipForward 
+attr_reader  :skipBackward
+  
   def initialize( moves )
     super()
     self.caption = 'Hive Boardgame view'
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    
+    @next = Gosu::Image.new(self, GameStepView.BASE_RESOURCE_PATH + "/forward.png",true)
+    @previous = Gosu::Image.new(self, GameStepView.BASE_RESOURCE_PATH + "/previous.png",true)
+    @skipForward = Gosu::Image.new(self, GameStepView.BASE_RESOURCE_PATH + "/fast_forward.png",true)
+    @skipBackward = Gosu::Image.new(self, GameStepView.BASE_RESOURCE_PATH + "/fast_back.png",true)
     
     #@zoom_factor = 0.7
     #@piece_spacing = 3.0
@@ -27,11 +37,21 @@ class Hive::GameStepView < Hive::GameView
   end
 
   def draw
-  	# draw_background
+  	draw_background
   	#    draw_grid
   	#    @pieces.each { |pg| pg.draw } 
   	#    @font.draw("#{ @game.get_state_description }", 10, 10, 12, 1.0, 1.0, -1)
   end
+  
+  def draw_background
+     draw_quad(
+      0,     0,      Hive::ViewConstants::TOP_COLOR,
+      Hive::ViewConstants::WINDOW_WIDTH, 0,      Hive::ViewConstants::TOP_COLOR,
+      0,     Hive::ViewConstants::WINDOW_HEIGHT, Hive::ViewConstants::BOTTOM_COLOR,
+      Hive::ViewConstants::WINDOW_WIDTH, Hive::ViewConstants::WINDOW_HEIGHT, Hive::ViewConstants::BOTTOM_COLOR,
+      0)
+  end
+  
 
   def draw_grid
     # i = 0
